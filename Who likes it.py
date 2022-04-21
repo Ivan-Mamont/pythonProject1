32fad41dca51dccdb3b9b49d67e2a2201373371b
Ivan-Mamont <ivanaks2000@gmail.com>
2022-04-21 17:27:50 +0300

    Finish

---
+++ Who likes it.py
@@ -0,0 +1,22 @@
+def likes(names):
+
+    d = {
+        0: "no one likes this",
+        1: "{} likes this",
+        2: "{} and {} like this",
+        3: "{}, {} and {} like this",
+        4: "{}, {} and {others} others like this"
+        }
+    length = len(names)
+    return d[min(4, length)].format(*names, others = length - 2)
+
+
+print(likes([])) #'no one likes this'
+
+print(likes(['Peter'])) #'Peter likes this'
+
+print(likes(['Jacob', 'Alex'])) # 'Jacob and Alex like this')
+
+print(likes(['Max', 'John', 'Mark'])) #'Max, John and Mark like this'
+
+print(likes(['Alex', 'Jacob', 'Mark', 'Max'])) #'Alex, Jacob and 2 others like this'
