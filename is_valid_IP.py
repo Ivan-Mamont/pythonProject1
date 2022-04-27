61b5dacc25c2ea2ae771ffe576f3fa3ecbaccae6
Ivan-Mamont <ivanaks2000@gmail.com>
2022-04-27 10:41:38 +0300

    Finish

--- is_valid_IP.py
+++ is_valid_IP.py
@@ -3,7 +3,7 @@ import re
 def is_valid_IP(strng):
     massiv = strng.split('.')
     pattern = r'\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}'
-    pattern2 = r'[0]\d{1,2}'
+    pattern2 = r'[0]\d{2}|[0][0]\d{1}|[.][0]\d{1}|^[0]\d+'
     if re.search(pattern2, strng):
         print('1',False)
         return False
@@ -19,3 +19,14 @@ def is_valid_IP(strng):
 
 
 is_valid_IP('147.207.139.237')
+is_valid_IP('12.255.56.1') #, True)
+is_valid_IP('')#, False)
+is_valid_IP('abc.def.ghi.jkl')#, False)
+is_valid_IP('12.34.56')#, False)
+is_valid_IP('12.34.56 .1')#, False)
+is_valid_IP('12.34.56.-1')#, False)
+is_valid_IP('123.045.067.089')#, False)
+is_valid_IP('127.1.1.0')#, True)
+is_valid_IP('0.0.0.0')#, True)
+is_valid_IP('0.34.82.53')#, True)
+is_valid_IP('192.168.1.300')#, False)
